3ed8732c1bca42fb6373d705ce24096a03035ec5
Jeffrey Young <jeffreymiyoung@aol.com>
2022-06-12 16:28:58 -0400

    added  main/comment/post handlebars

---
+++ server.js
@@ -0,0 +1,50 @@
+// imports
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+
+// express handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// express-session and cookies section
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    // "Super secret secret" was original secret: value process.env.SECRET
+    secret: 'whisperwhisperwhisper',
+    cookie: {
+        // Session expires in 10 minutes.
+        expires: 10 * 60 * 1000
+    },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+// app
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+// express-handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening!'));
+});
