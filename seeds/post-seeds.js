47f40c99fd36fbfe96f831940e3287b672a34d37
Jeffrey Young <jeffreymiyoung@aol.com>
2022-06-12 16:55:04 -0400

    added partials handlebars and homepage/dashboard handlebars

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,17 +4,17 @@ const { Post } = require('../models');
 const postData = [
     {
         title: 'Advanced quantum computer made available to the public for first time',
-        post_url: 'https://www.newscientist.com/article/2322807-advanced-quantum-computer-made-available-to-the-public-for-first-time/',
+        post_content: 'Words for Example',
         user_id: 1,
     },
     {
         title: 'Mini nuclear power stations may produce more waste than large ones',
-        post_url: 'https://www.newscientist.com/article/2322252-mini-nuclear-power-stations-may-produce-more-waste-than-large-ones/',
+        post_content: 'Text for Example',
         user_id: 1,
     },
     {
         title: 'Synthetic membrane could lead to self-replicating artificial cells',
-        post_url: 'https://www.newscientist.com/article/2322222-synthetic-membrane-could-lead-to-self-replicating-artificial-cells/',
+        post_content: 'Speech for Example',
         user_id: 1,
     },
 ];
